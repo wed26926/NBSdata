4bacab1fa6b835b92f486300dce3bc83361cfce2
wed26926 <35561100+wed26926@users.noreply.github.com>
2019-06-17 17:28:53 +0800

    Update initializeTradesAndProvince.go

--- commonTools/initializeTradesAndProvince.go
+++ commonTools/initializeTradesAndProvince.go
@@ -1,15 +1,24 @@
 package commonTools
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"os"
 	"strings"
 )
 
+type TradeTree struct{
+	Name   string      `json:"name"`
+	Id string          `json:"id"`
+	Childs []TradeTree `json:"childs"`
+}
+
 var province map[string]string
+var root TradeTree
 
 func init() {
 	initializeProvince()
+	initializeTrade()
 }
 
 func initializeProvince(){
@@ -25,6 +34,20 @@ func initializeProvince(){
 	}
 }
 
+func initializeTrade(){
+	content,err := ioutil.ReadFile(os.Getenv("GOPATH")+"/src/NBSdata/commonTools/trades.txt")
+	if err != nil{
+		panic(err)
+	}
+	err = json.Unmarshal(content,&root)
+	if err != nil{
+		panic(err)
+	}
+}
 func  GetProvinceData() map[string]string{
 	return province
+}
+
+func GetTrade() TradeTree {
+	return root
 }
