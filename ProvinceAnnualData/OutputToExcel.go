9f4d2eef879e275be1d2d83f59a5f25cccf9e533
wed26926 <35561100+wed26926@users.noreply.github.com>
2019-06-25 11:47:30 +0800

    Create OutputToExcel.go

---
+++ ProvinceAnnualData/OutputToExcel.go
@@ -0,0 +1,35 @@
+package ProvinceAnnualData
+
+import (
+	"github.com/tealeg/xlsx"
+	"log"
+)
+
+func OutputToExcel(response Response){
+	xlsxfile := xlsx.NewFile()
+	sheet,err := xlsxfile.AddSheet("指标")
+	if err != nil{
+		log.Println(err)
+	}
+	row1 := sheet.AddRow()
+	zbnames := make([]map[string]string,len(response.Returndata.Wdnodes))
+	zblocations := make(map[string]int)
+	for i,wd := range response.Returndata.Wdnodes{
+		row1.AddCell().Value = wd.Wdname
+		zblocations[wd.Wdcode] = i
+		zbnames[i] = make(map[string]string)
+		for _,wdnode := range wd.Nodes{
+			zbnames[i][wdnode.Code] = wdnode.Name
+		}
+	}
+	for _,data := range response.Returndata.Datanodes{
+		row := sheet.AddRow()
+		row.Cells = make([]*xlsx.Cell,len(zbnames))
+		for _,wd := range data.Wds{
+			row.Cells[zblocations[wd.Wdcode]] = new(xlsx.Cell)
+			row.Cells[zblocations[wd.Wdcode]].Value = zbnames[zblocations[wd.Wdcode]][wd.Valuecode]
+		}
+		row.AddCell().SetFloat(data.Data.Data)
+	}
+	xlsxfile.Save("test.xlsx")
+}
